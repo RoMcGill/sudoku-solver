daeae02896aae4fd5af1b1f25a663cfe8cb25c3b
RoMcGill <ro.mcgill12@gmail.com>
2022-04-01 13:55:31 +0000

    create sudoku grid and  function to print sudoku grid

--- run.py
+++ run.py
@@ -1,3 +1,29 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# sudoku board to solve 
+grid = [
+    [7,8,0,4,0,0,1,2,0],
+    [6,0,0,0,7,5,0,0,9],
+    [0,0,0,6,0,1,0,7,8],
+    [0,0,7,0,4,0,2,6,0],
+    [0,0,1,0,5,0,9,3,0],
+    [9,0,4,0,6,0,0,0,5],
+    [0,7,0,3,0,0,0,1,2],
+    [1,2,0,0,0,7,4,0,0],
+    [0,4,9,2,0,6,0,0,7],
+]
+
+
+def print_grid(grid):
+    for i in range(len(grid)):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - - -")
+
+        for j in range(len(grid[0])):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end="")
+
+            if j ==8:
+                print(grid[i][j])
+            else:
+                print(str(grid[i][j]) + " ",end="")
+
+print_grid(grid)
