08457b86d522dba25f1cdf71b0c76fae5c8e7759
RoMcGill <ro.mcgill12@gmail.com>
2022-04-01 14:07:42 +0000

    add function to find (0) which will be used to represent an empty box on the grid

--- run.py
+++ run.py
@@ -1,18 +1,21 @@
-# sudoku board to solve 
-grid = [
-    [7,8,0,4,0,0,1,2,0],
-    [6,0,0,0,7,5,0,0,9],
-    [0,0,0,6,0,1,0,7,8],
-    [0,0,7,0,4,0,2,6,0],
-    [0,0,1,0,5,0,9,3,0],
-    [9,0,4,0,6,0,0,0,5],
-    [0,7,0,3,0,0,0,1,2],
-    [1,2,0,0,0,7,4,0,0],
-    [0,4,9,2,0,6,0,0,7],
+# sudoku board to solve
+sudoku_grid = [
+    [7, 8, 0, 4, 0, 0, 1, 2, 0],
+    [6, 0, 0, 0, 7, 5, 0, 0, 9],
+    [0, 0, 0, 6, 0, 1, 0, 7, 8],
+    [0, 0, 7, 0, 4, 0, 2, 6, 0],
+    [0, 0, 1, 0, 5, 0, 9, 3, 0],
+    [9, 0, 4, 0, 6, 0, 0, 0, 5],
+    [0, 7, 0, 3, 0, 0, 0, 1, 2],
+    [1, 2, 0, 0, 0, 7, 4, 0, 0],
+    [0, 4, 9, 2, 0, 6, 0, 0, 7],
 ]
 
 
 def print_grid(grid):
+    """
+    function to print sudoku grid
+    """
     for i in range(len(grid)):
         if i % 3 == 0 and i != 0:
             print("- - - - - - - - - - - - -")
@@ -21,9 +24,18 @@ def print_grid(grid):
             if j % 3 == 0 and j != 0:
                 print(" | ", end="")
 
-            if j ==8:
+            if j == 8:
                 print(grid[i][j])
             else:
-                print(str(grid[i][j]) + " ",end="")
+                print(str(grid[i][j]) + " ", end="")
 
-print_grid(grid)
+
+def find_zero(grid):
+    """
+    function to find zero in grid,
+    zero will be used to represent an empty space
+    """
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 0:
+                return (i, j)
