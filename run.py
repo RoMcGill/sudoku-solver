f7389a5e66f40c7141775a57e8ad4e1cb778c6b3
RoMcGill <ro.mcgill12@gmail.com>
2022-04-01 14:43:28 +0000

    add function to check if the game is correct/valid and add function to backtrack true numbers for each square and loop through until correct depending on surrounding numbers

--- run.py
+++ run.py
@@ -11,6 +11,49 @@ sudoku_grid = [
     [0, 4, 9, 2, 0, 6, 0, 0, 7],
 ]
 
+def solve(sudoku_grid):
+    """
+    function to backtrack by calling the function inside its self
+    our completed sudoku grid will be the basecase for the recursion 
+    """
+    find = find_zero(sudoku_grid)
+    #if this is true the grid has been solved 
+    if not find:
+        return True
+    else:
+        row, col = find
+
+    for i in range(1,10):
+        if correct(sudoku_grid, i, (row, col)):
+            sudoku_grid[row][col] = i
+            
+            if solve (sudoku_grid):
+                return True
+            sudoku_grid[row][col] = 0 
+def correct(sudoku_grid, num, pos):
+    """
+    finds if the current game is correct or valid
+    """
+    # check row
+    for i in range(len(sudoku_grid[0])):
+        if sudoku_grid[pos[0]][i] == num and pos[1] != i:
+            return False
+
+    # check column
+    for i in range(len(sudoku_grid)):
+        if sudoku_grid[i][pos[1]] == num and pos[0] != i:
+            return False
+
+    # check square
+    square x = pos [1] // 3
+    square y = pos [0] // 3
+    for i in range(square_y * 3, square_y * 3 + 3):
+        for j in range (square_x * 3, square_x * 3 + 3):
+            if sudoku_grid[i][j] == num and (i,j) != pos:
+                return False
+
+    return True
+
 
 def print_grid(grid):
     """
@@ -39,3 +82,7 @@ def find_zero(grid):
         for j in range(len(grid[0])):
             if grid[i][j] == 0:
                 return (i, j)
+#if no zeros are left on the grid 
+    return None
+
+print_grid(sudoku_grid)
