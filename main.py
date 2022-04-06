672d6d9756d8da226412550528874595c276526e
RoMcGill <ro.mcgill12@gmail.com>
2022-04-06 20:11:22 +0000

    data not showing on sheet, but showing in analitics on google cloud platform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+
 import pyfiglet
 
 txt = pyfiglet.figlet_format("Sudoku Solver", font="big")
@@ -17,6 +18,27 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('sudoku')
 
+def get_name_data():
+    """
+    Get name figures input from the user.
+    Run a while loop to collect a valid string of data from the user
+    via the terminal, which must be a string of 6 numbers separated
+    by commas. The loop will repeatedly request data, until it is valid.
+    """
+    print("Please enter your name .")
+    data_str = input("Enter your name here: ")
+
+       
+def update_name_worksheet(data):
+    """
+    Update name worksheet, add new row with the list data provided
+    """
+    print("Updating name worksheet...\n")
+    name_worksheet = SHEET.worksheet("name")
+    name_worksheet.append_row(data)
+    print("name worksheet updated successfully.\n")
 
 
-name = SHEET.worksheet('name')
+data = get_name_data()
+update_name_worksheet(data)
+
