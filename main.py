2a6b99b074beb1a279d0b3b15e0a2c2e883009f1
RoMcGill <ro.mcgill12@gmail.com>
2022-04-09 11:23:04 +0000

    add main function to call functions and to stop global scope of data = in the main.py file

--- main.py
+++ main.py
@@ -1,44 +1,86 @@
 import gspread
 from google.oauth2.service_account import Credentials
-
 import pyfiglet
 
-txt = pyfiglet.figlet_format("Sudoku Solver", font="big")
-solved = pyfiglet.figlet_format("solved", font="big")
-print(txt)
 
-SCOPE = [    
+
+SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive.file",    
+    "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
     ]
 
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('sudoku')
+SHEET = GSPREAD_CLIENT.open('project3')
 
-def get_name_data():
+
+def get_puzzel_data():
     """
-    Get name figures input from the user.
+    Get puzzel figures input from the user.
     Run a while loop to collect a valid string of data from the user
-    via the terminal, which must be a string of 6 numbers separated
+    via the terminal, which must be a string of 9 numbers separated
     by commas. The loop will repeatedly request data, until it is valid.
     """
-    print("Please enter your name .")
-    data_str = input("Enter your name here: ")
+    while True:
+        print("Please enter your puzzel data.")
+        print("Data should be 9 numbers, separated by commas.")
+        print("Example: 1,0,0,4,7,3,9,8,7\n")
 
-       
-def update_name_worksheet(data):
+        data_str = input("Enter your data here: ")
+
+
+
+        puzzel_data = data_str.split(",")
+
+        if validate_data(puzzel_data):
+            print("puzzel is valid!")
+            break
+
+    return puzzel_data
+
+
+def validate_data(values):
     """
-    Update name worksheet, add new row with the list data provided
+    Inside the try, converts all string values into integers.
+    Raises ValueError if strings cannot be converted into int,
+    or if there aren't exactly 9 values.
     """
-    print("Updating name worksheet...\n")
-    name_worksheet = SHEET.worksheet("name")
-    name_worksheet.append_row(data)
-    print("name worksheet updated successfully.\n")
+    try:
+        [int(value) for value in values]
+        if len(values) != 9:
+            raise ValueError(
+                f"Exactly 9 values required, you provided {len(values)}"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
+        return False
+
+    return True
 
 
-data = get_name_data()
-update_name_worksheet(data)
+def update_puzzel_worksheet(data):
+    """
+    Update puzzel worksheet, add new row with the list data provided
+    """
+    get_puzzel_data()
+    print("Updating puzzel worksheet...\n")
+    puzzel_worksheet = SHEET.worksheet("puzzel")
+    puzzel_worksheet.append_row(data)
 
+    print("puzzel worksheet updated successfully.\n")
+
+
+
+
+def main():
+    data = get_puzzel_data()
+    puzzel_data = [int(num) for num in data]
+    update_puzzel_worksheet(puzzel_data)
+
+txt = pyfiglet.figlet_format("Sudoku Solver", font="big")
+solved = pyfiglet.figlet_format("solved", font="big")
+print(txt)
+
+main()
